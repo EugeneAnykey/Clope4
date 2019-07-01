23da903d5a04fbd77736b998b7eff3b00f0079dc
Eugene <evgeniy.alyabyev@loginom.ru>
2019-07-01 11:01:27 +0300

    changed order

--- Clope/ClopeWin/FilesHelper.cs
+++ Clope/ClopeWin/FilesHelper.cs
@@ -24,17 +24,17 @@ namespace ClopeWin
 				new DelimitedFile[] {
 					new DelimitedFile("empty.txt", commaTabSep),
 					new DelimitedFile("agaricus-lepiota.csv", commaSep),
+					new DelimitedFile("mushrooms.txt", tabSep) { FirstLinesToSkip = 1 },
+					new DelimitedFile("megapolis_clean_quanted.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("ex_article.txt", commaSep),
 					new DelimitedFile("ex_a1.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("ex_a2.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("ex_a3.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("lines_01.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("lines_02.txt", tabSep) { FirstLinesToSkip = 1 },
-					new DelimitedFile("megapolis_clean_quanted.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("megapolis_clean_approx_1.txt", tabSep) { FirstLinesToSkip = 1 },
 					new DelimitedFile("agaricus-lepiota.safe", commaSep),
 					new DelimitedFile("agaricus-lepiota.small", commaSep),
-					new DelimitedFile("mushrooms.txt", tabSep) { FirstLinesToSkip = 1 },
 				}
 			);
 
