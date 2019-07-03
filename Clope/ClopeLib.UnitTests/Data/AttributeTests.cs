508ad59e009033b7b5b644a2ad95df8d5031d134
Eugene <evgeniy.alyabyev@loginom.ru>
2019-07-03 14:46:10 +0300

    Update AttributeTests.cs

--- Clope/ClopeLib.UnitTests/Data/AttributeTests.cs
+++ Clope/ClopeLib.UnitTests/Data/AttributeTests.cs
@@ -25,5 +25,39 @@ namespace ClopeLib.UnitTests.Data
 				at.Name
 			);
 		}
+
+
+
+		[Test]
+		public void Equals_Self_IsTrue()
+		{
+			IAttribute at = new Attribute4(2, "f");
+
+			Assert.IsTrue(at.Equals(at));
+		}
+
+		[TestCase(0, "a", 0, "a")]
+		[TestCase(1, "a", 1, "a")]
+		[TestCase(1, "z", 1, "z")]
+		public void Equals_Similar_IsTrue(int pos1, string name1, int pos2, string name2)
+		{
+			IAttribute at1 = new Attribute4(pos1, name1);
+			IAttribute at2 = new Attribute4(pos2, name2);
+
+			Assert.IsTrue(at1.Equals(at2));
+		}
+
+
+
+		[TestCase(0, "a", 0, "z")]
+		[TestCase(0, "a", 6, "a")]
+		[TestCase(0, "a", 6, "z")]
+		public void Equals_Different_IsFalse(int pos1, string name1, int pos2, string name2)
+		{
+			IAttribute at1 = new Attribute4(pos1, name1);
+			IAttribute at2 = new Attribute4(pos2, name2);
+
+			Assert.IsFalse(at1.Equals(at2));
+		}
 	}
 }
