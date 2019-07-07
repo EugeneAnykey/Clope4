7bd2817dd271964c5134d1062228c3ad5d8fe68c
Eugene Anykey <udjin666@gmail.com>
2019-07-08 01:30:34 +0300

    added IndexCounters

---
+++ Clope/ClopeLib/Data4/IndexCounterAtArray.cs
@@ -0,0 +1,54 @@
+﻿namespace ClopeLib.Data
+{
+	public class IndexCounterAtArray : IIndexCounter
+	{
+		int[] counter = new int[0];
+		public int Positives { get; private set; } = 0;
+
+
+
+		// interface
+		public void Inc(int[] indicies)
+		{
+			foreach (var index in indicies)
+			{
+				if (index >= counter.Length)
+					Expand(index);
+
+				var temp = counter[index];
+				counter[index]++;
+
+				if (counter[index] == 1)
+					Positives++;
+			}
+		}
+
+		public void Dec(int[] indicies)
+		{
+			foreach (var index in indicies)
+			{
+				if (index >= counter.Length)
+					Expand(index);
+
+				var temp = counter[index];
+				counter[index]--;
+
+				if (counter[index] == 0)
+					Positives--;
+			}
+		}
+
+
+
+		public int this[int index] => 0 <= index && index < counter.Length ? counter[index] : 0;
+
+
+
+		void Expand(int index)
+		{
+			var temp = new int[index + 1];
+			counter.CopyTo(temp, 0);
+			counter = temp;
+		}
+	}
+}
---
+++ Clope/ClopeLib/_root/IIndexCounter.cs
@@ -0,0 +1,13 @@
+﻿namespace ClopeLib
+{
+	public interface IIndexCounter
+	{
+		void Inc(int[] indicies);
+
+		void Dec(int[] indicies);
+
+		int Positives { get; }
+
+		int this[int index] { get; }
+	}
+}
